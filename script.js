af359403fce75fd45d98330bb0ab745340d7edd6
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-14 20:50:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputInitialPrice = document.querySelector("#initial-price");
+const inputQuantity = document.querySelector("#quantity");
+const inputCurrentPrice = document.querySelector("#current-price");
+const btnTellMe = document.querySelector("#btn-tell-me");
+const outputBox = document.querySelector("#output-box");
+
+const percentage = (obtained, total) => (obtained / total) * 100;
+
+const calculate = (initialPrice, currentPrice, quantity) => {
+  const totalInvested = initialPrice * quantity;
+  const difference = currentPrice - initialPrice;
+  let message = "";
+  if (difference === 0) {
+    message = "No profit No Loss, You just need to care about taxes 🤔️";
+  } else if (difference > 0) {
+    // work for profit
+    const profitAmount = quantity * difference;
+    const profitPercentage = percentage(profitAmount, totalInvested);
+    message = `profit amount : ${profitAmount} profit percentage : ${profitPercentage}`;
+  } else {
+    // work for loss
+    const lossAmount = quantity * difference * -1;
+    const lossPercentage = percentage(lossAmount, totalInvested);
+    message = `loss amount : ${lossAmount} loss percentage : ${lossPercentage}`;
+  }
+  setMessage(message);
+};
+
+function setMessage(message) {
+  outputBox.innerHTML = message;
+}
+
+btnTellMe.addEventListener("click", () => {
+  const initialPrice = inputInitialPrice.value;
+  const currentPrice = inputCurrentPrice.value;
+  const quantity = inputQuantity.value;
+
+  if (initialPrice == "" || currentPrice == "" || quantity == "")
+    setMessage("Please fill all the fields.");
+  else calculate(initialPrice, currentPrice, quantity);
+});
